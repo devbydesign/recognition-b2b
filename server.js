96ce6cbf26d78e17b273e8d5bb6cfcfbe01b4cca
aredj <aredja2@gmail.com>
2025-07-28 13:27:50 -0600

    many color updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { URL } = require('url'); // Import URL module for decoding
 
-const port = 3003;
+const port = 3004;
 
 // MIME types for different file extensions
 const mimeTypes = {
